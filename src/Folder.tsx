6a85cbf897889a8465b24c0b79a895d2e66c9757
Miguel Amores <mamores@waverleysoftware.com>
2025-01-27 14:38:23 -0500

    show file icons

--- src/Folder.tsx
+++ src/Folder.tsx
@@ -2,6 +2,10 @@ import React, { Fragment, useState } from "react";
 import Icon from "./Icon";
 import Item from "./Item";
 
+function getIconName(file: string) {
+  return file.split(".").pop();
+}
+
 const Folder = ({ files }: { files: FileType | undefined }) => {
   return (
     <ul className="list">
@@ -9,6 +13,7 @@ const Folder = ({ files }: { files: FileType | undefined }) => {
       {files?.isFolder &&
         files.children?.map((item: FileType) => (
           <li key={item.name} className="item">
+            {!item.isFolder && <Icon name={getIconName(item.name)} />}
             <Folder files={item} />
           </li>
         ))}
