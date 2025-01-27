e2052b3090e731a4a943a451c0ba3a0c8f3728bf
Miguel Amores <mamores@waverleysoftware.com>
2025-01-27 14:50:15 -0500

    add expand folder

--- src/Folder.tsx
+++ src/Folder.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import { useState } from "react";
 import Icon from "./Icon";
 import Item from "./Item";
 
@@ -7,10 +7,17 @@ function getIconName(file: string) {
 }
 
 const Folder = ({ files }: { files: FileType | undefined }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const showChildren = files?.isFolder && expanded;
+
+  const handleExpand = () => setExpanded((prev) => !prev);
   return (
     <ul className="list">
-      <p>{files?.name}</p>
-      {files?.isFolder &&
+      <button onClick={handleExpand}>
+        <p>{files?.name}</p>
+      </button>
+      {showChildren &&
         files.children?.map((item: FileType) => (
           <li key={item.name} className="item">
             {!item.isFolder && <Icon name={getIconName(item.name)} />}
